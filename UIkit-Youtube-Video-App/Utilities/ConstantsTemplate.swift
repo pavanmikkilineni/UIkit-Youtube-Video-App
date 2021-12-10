b389817e5642be7232665ed2f5f9a804b8df3d98
MIKKILINENI PAVAN KUMAR <pavanmikkilineni690@gmail.com>
2021-12-09 21:57:34 -0800

    Added ConstantsTemplate

---
+++ UIkit-Youtube-Video-App/Utilities/ConstantsTemplate.swift
@@ -0,0 +1,20 @@
+//
+//  ConstantsTemplate.swift
+//  UIkit-Youtube-Video-App
+//
+//  Created by Pavan Mikkilineni on 09/12/21.
+//
+
+/* Add your own api key below and uncomment the code.*/
+
+import Foundation
+
+/*
+struct Constants{
+    
+    static var API_KEY=""
+    static var PLAYLIST_ID="PLMRqhzcHGw1aLoz4pM_Mg2TewmJcMg9ua"
+    static var API_URL=" https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+
+*/
